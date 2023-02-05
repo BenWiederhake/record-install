0e2f0a65947468b23a16b67d2021a78987ea84a8
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2023-02-05 23:20:15 +0100

    Formalize tests slightly, fix few edgecases

--- parse_arg.py
+++ parse_arg.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/bin/false
+# This is a library
 
 import logging
 import inspect
@@ -13,7 +14,7 @@ import lark
 # ("struct", re.compile(r'^(\{[^}]+\})(?:, |$)')),
 # ("flagset", re.compile(r'^(~?\[[A-Z0-9_ ]*\])(?:, |$)')),
 # ("dents", re.compile(r'^(0x[0-9a-f]+) /\* ([0-9]+) entries \*/(?:, |$)')),
-int_tree_grammar = r"""
+arg_list_grammar = r"""
     arg_list: (value (", " value)*)?
     ?value: atom
     ?atom: "(" value ("," value)* ")"         -> tuple
@@ -25,15 +26,14 @@ int_tree_grammar = r"""
          | DEC_NUMBER                         -> dec_number
          | HEX_NUMBER                         -> hex_number
          | STRING [complete]                  -> string
-    //     | "..."                              -> dots
     complete: "..."
     key_value: ID "=" value
-    ID: /\w+/
-    FD_START.2: /\d+</
-    FD_MAIN: /[^\n<>]+/
+    ID: /[A-Z0-9_]+/
+    FD_START.1: /\d+</
+    FD_MAIN: /[^<>]+/
     FD_META: /<[^>]*>/
-    HEX_NUMBER.1: /0x[\dA-Fa-f]+(?!\w)/
-    DEC_NUMBER.1: /-?\d+(?!\w)/
+    HEX_NUMBER: /0x[\dA-Fa-f]+(?!\w)/
+    DEC_NUMBER: /-?(0(?!x)|[1-9][0-9]*)(?!x)/ // \d+(?!\w)
     STRING: /"([^\\\\"]|\\.)*"/
 """
 
@@ -46,216 +46,69 @@ ESCAPE_CHAR_TO_CHAR = {
     '"': '"',
     "\\": "\\",
 }
-
-lark.logger.setLevel(logging.DEBUG)
-inline_args = lark.v_args(inline=True)
+OCTAL_DIGITS = "01234567"
 
 
 def see(x):
-    print(f"{inspect.stack()[1][3]} sees {repr(x)}")
+    print(f"{inspect.stack()[1][3]} sees {repr(x)} = '{x}'")
 
 
-class MyTransformer(lark.Transformer):
-    arg_list = list
+class EscapeError(Exception):
+    pass
 
-    def prefix(self, args):
-        return len(args)
 
-    @inline_args
-    def arg_ident(self, identifier):
-        return {"type": "identifier", "name": identifier}
+@lark.v_args(inline=True)
+class ArgListTransformer(lark.Transformer):
+    def arg_list(self, *children):
+        return list(children)
 
-    def arg_bitset(self, identifiers):
-        return {"type": "bitset", "values": identifiers}
+    def identifier(self, name):
+        return {"type": "identifier", "name": name.value}
 
-    @inline_args
-    def arg_neg_int_b10(self, value):
-        return {"type": "int_b10", "value": -value}
+    def bitset(self, *values):
+        return {"type": "bitset", "values": [v.value for v in values]}
 
-    @inline_args
-    def arg_uint_b16(self, value):
-        return {"type": "uint_b16", "value": value}
+    def dec_number(self, value):
+        return {"type": "int_b10", "value": int(value)}
 
-    @inline_args
-    def token_value(self, token):
-        return token.value
+    def hex_number(self, value):
+        return {"type": "uint_b16", "value": int(value[2:], 16)}
 
-    @inline_args
-    def uint_b10(self, digits):
-        return int(digits.value)
-
-    @inline_args
-    def uint_b16(self, digits):
-        return int(digits.value, 16)
-
-    def maybe_string_continuation(self, parts):
-        assert parts == [] or parts == ["..."]
-        if parts:
-            return "incomplete"
-        else:
-            return "complete"
-
-    @inline_args
-    def escaped_character(self, char):
-        return ESCAPE_CHAR_TO_CHAR[char.value]
-
-    def escaped_string(self, parts):
-        str_parts = []
-        for i, p in enumerate(parts):
-            if isinstance(p, str):
-                str_parts.append(p)
+    def STRING(self, token):
+        token = token[1:-1]
+        out = ""
+        i = 0
+        while i < len(token):
+            if token[i] == "\\":
+                i += 1
+                if token[i] in OCTAL_DIGITS:
+                    l = 1
+                    if token[i + 1] in OCTAL_DIGITS:
+                        l += 1
+                        if token[i + 2] in OCTAL_DIGITS:
+                            l += 1
+                        elif token[i] == "0":
+                            raise EscapeError(f"Invalid octal code {token[i : i+3]!r}")
+                    out += chr(int(token[i:i + l], 8))
+                    i += l
+                elif token[i] in ESCAPE_CHAR_TO_CHAR:
+                    out += ESCAPE_CHAR_TO_CHAR[token[i]]
+                    i += 1
+                else:
+                    raise EscapeError(f"Unknown escape sequence {token[i : i+3]!r}")
             else:
-                print(f"ERROR: part#{i + 1} is not a str: >>{p}<<")
-        return "".join(str_parts)
+                out += token[i]
+                i += 1
+        return out
 
-    @inline_args
-    def numeric_character(self, char):
-        return chr(int(char.value, 8))
+    def string(self, value, complete_marker):
+        return {"type": "string", "value": value, "complete": not complete_marker}
 
-    @inline_args
-    def from_common_escape(self, string):
-        return string
-
-    @inline_args
-    def arg_string(self, string, rest):
-        assert rest in ["complete", "incomplete"]
-        return {
-            "type": "string",
-            "value": string,
-            "complete": rest == "complete",
-        }
-
-    @inline_args
-    def arg_path_or_int_b10(self, fd, path=None, metadata=None):
-        assert path is not None or metadata is None  # "metadata implies path"
-        if path is None:
-            return {"type": "int_b10", "value": fd}
-        return {
-            "type": "fd",
-            "value": fd,
-            "path": path,
-            "metadata": metadata,
-        }
+    def fd(self, start, main, meta):
+        return {"type": "fd", "value": int(start[:-1]), "path": self.STRING('"' + main + '"'),
+                "metadata": None if meta is None else meta[1:-1]}
 
 
-TRANSFORMER = MyTransformer()
-
-arg_list_parser = lark.Lark(int_tree_grammar, start="arg_list", parser="lalr", debug=True)
-
-
-def run_expectation(test_index, input_string, expected):
-    print(f"[P{test_index:03}] Testing '{input_string}' ... ", end="")
-    try:
-        actual = arg_list_parser.parse(input_string)
-        #actual = TRANSFORMER.transform(arg_list_parser.parse(input_string))
-    except ValueError as e:
-        print(f"\nERROR: {e}")
-        return
-    if actual != expected:
-        print()
-        print(f"ERROR: Expected {expected} instead")
-        print(f"       Actual = {actual}")
-        print(actual.pretty())
-        return
-    print("ok")
-
-
-EXPECTATIONS = [
-    ('', []),
-    ('FOO', [{"type": "identifier", "name": "FOO"}]),
-    ('FOO, BAR', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}]),
-    ('FOO, BAR, BAZ', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}, {"type": "identifier", "name": "BAZ"}]),
-    ('FOO, BAR, BAZ, QUUX', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}, {"type": "identifier", "name": "BAZ"}, {"type": "identifier", "name": "QUUX"}]),
-    ('FOO|BAR', [{"type": "bitset", "values": ["FOO", "BAR"]}]),
-    ('FOO|BAR|BAZ', [{"type": "bitset", "values": ["FOO", "BAR", "BAZ"]}]),
-    ('FOO|BAR, BAZ|QUUX', [{"type": "bitset", "values": ["FOO", "BAR"]}, {"type": "bitset", "values": ["BAZ", "QUUX"]}]),
-    ('0', [{"type": "int_b10", "value": 0}]),
-    ('123', [{"type": "int_b10", "value": 123}]),
-    ('-1234567', [{"type": "int_b10", "value": -1234567}]),
-    ('9223372036854775807', [{"type": "int_b10", "value": 9223372036854775807}]),
-    ('-9223372036854775808', [{"type": "int_b10", "value": -9223372036854775808}]),
-    ('0x0', [{"type": "uint_b16", "value": 0x0}]),
-    ('0x123', [{"type": "uint_b16", "value": 0x123}]),
-    ('0x1234567', [{"type": "uint_b16", "value": 0x1234567}]),
-    ('0x123456789abcdef0', [{"type": "uint_b16", "value": 0x123456789abcdef0}]),
-    ('"hello"', [{"type": "string", "value": "hello", "complete": True}]),
-    ('"FOO"', [{"type": "string", "value": "FOO", "complete": True}]),
-    ('"0asdf"', [{"type": "string", "value": "0asdf", "complete": True}]),
-    ('"qwer0asdf"', [{"type": "string", "value": "qwer0asdf", "complete": True}]),
-    ('""', [{"type": "string", "value": "", "complete": True}]),
-    ('"world"...', [{"type": "string", "value": "world", "complete": False}]),
-    ('""...', [{"type": "string", "value": "", "complete": False}]),
-    # This is a really wacky encoding.
-    (r'"really\1weird"', [{"type": "string", "value": "really\x01weird", "complete": True}]),
-    (r'"really\10weird"', [{"type": "string", "value": "really\x08weird", "complete": True}]),
-    (r'"really\100weird"', [{"type": "string", "value": "really\x40weird", "complete": True}]),
-    (r'"really\0010weird"', [{"type": "string", "value": "really\x010weird", "complete": True}]),
-    (r'"really\0100weird"', [{"type": "string", "value": "really\x080weird", "complete": True}]),
-    (r'"really\1000weird"', [{"type": "string", "value": "really\x400weird", "complete": True}]),
-    (r'"really\tweird"', [{"type": "string", "value": "really\x09weird", "complete": True}]),
-    (r'"really\nweird"', [{"type": "string", "value": "really\x0aweird", "complete": True}]),
-    (r'"really\vweird"', [{"type": "string", "value": "really\x0bweird", "complete": True}]),
-    (r'"really\fweird"', [{"type": "string", "value": "really\x0cweird", "complete": True}]),
-    (r'"really\rweird"', [{"type": "string", "value": "really\x0dweird", "complete": True}]),
-    (r'"really\"weird"', [{"type": "string", "value": "really\x22weird", "complete": True}]),
-    (r'"really\\weird"', [{"type": "string", "value": "really\x5cweird", "complete": True}]),
-    (r'"really\177weird"', [{"type": "string", "value": "really\x7fweird", "complete": True}]),
-    (r'"really\200weird"', [{"type": "string", "value": "really\x80weird", "complete": True}]),
-    (r'"really\234weird"', [{"type": "string", "value": "really\x9cweird", "complete": True}]),
-    (r'"really\345weird"', [{"type": "string", "value": "really\xe5weird", "complete": True}]),
-    (r'"really\377weird"', [{"type": "string", "value": "really\xffweird", "complete": True}]),
-    (r'"really\08weird"', [{"type": "string", "value": "really\x008weird", "complete": True}]),
-    (r'"really\18weird"', [{"type": "string", "value": "really\x018weird", "complete": True}]),
-    (r'"really\28weird"', [{"type": "string", "value": "really\x028weird", "complete": True}]),
-    (r'"really\38weird"', [{"type": "string", "value": "really\x038weird", "complete": True}]),
-    (r'"really\48weird"', [{"type": "string", "value": "really\x048weird", "complete": True}]),
-    (r'"really\58weird"', [{"type": "string", "value": "really\x058weird", "complete": True}]),
-    (r'"really\68weird"', [{"type": "string", "value": "really\x068weird", "complete": True}]),
-    (r'"really\78weird"', [{"type": "string", "value": "really\x078weird", "complete": True}]),
-    (r'"really\108weird"', [{"type": "string", "value": "really\x088weird", "complete": True}]),
-    (r'"really\208weird"', [{"type": "string", "value": "really\x108weird", "complete": True}]),
-    (r'"really\218weird"', [{"type": "string", "value": "really\x118weird", "complete": True}]),
-    (r'"sanity<check>"', [{"type": "string", "value": "sanity<check>", "complete": True}]),
-    (r'"sanity,check>"', [{"type": "string", "value": "sanity,check>", "complete": True}]),
-    (r'"sanity check>"', [{"type": "string", "value": "sanity check>", "complete": True}]),
-    (r'"sanity, check>"', [{"type": "string", "value": "sanity, check>", "complete": True}]),
-    # fdstrings without the string cannot possibly recognized as such. We shift that responsibility to the user.
-    (r'1', [{"type": "int_b10", "value": 1}]),
-    (r'1</dev/null>', [{"type": "fd", "value": 1, "path": "/dev/null", "metadata": None}]),
-    (r'1</dev/null<char 1:3>>', [{"type": "fd", "value": 1, "path": "/dev/null", "metadata": "char 1:3"}]),
-    (r'3</tmp/x/THE\"MARKER>', [{"type": "fd", "value": 3, "path": "/tmp/x/THE\"MARKER", "metadata": None}]),
-]
-
-NEGATIVES = [
-    ('['),
-    (']'),
-    ('"'),
-    (', '),
-    ('FOO, '),
-    (', BAR'),
-    ('FOO,BAR'),
-    ('+7'),
-    ('-0x123'),
-    ('-3</tmp/foo>'),
-    ('0123'),
-    ('1234a'),
-    ('"hello\\"'),
-    ("'hello'"),
-    ('42...'),
-    (r'"really\03weird"'),
-    (r'"really\8weird"'),
-    (r'<barepath>'),
-    (r'<barepath<char 1:3>>'),
-]
-NEGATIVES = []  # FIXME
-
-for i, expectation in enumerate(EXPECTATIONS):
-    run_expectation(i, *expectation)
-for i, input_string in enumerate(NEGATIVES):
-    print(f"[N{i:03}] Testing '{input_string}' ... ", end="")
-    try:
-        tree = arg_list_parser.parse(input_string)
-    except (lark.exceptions.UnexpectedCharacters, lark.exceptions.UnexpectedToken, lark.exceptions.UnexpectedEOF):
-        print("ok")
-        continue
-    print(f"\nERROR: Was accepted?!\n{tree}\n{tree.pretty()}")
+lark.logger.setLevel(logging.DEBUG)
+TRANSFORMER = ArgListTransformer()
+arg_list_parser = lark.Lark(arg_list_grammar, start="arg_list", parser="lalr", debug=True)
---
+++ tests.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import lark
+import parse_arg
+import unittest
+
+EXPECTATIONS = [
+    ('', []),
+    ('FOO', [{"type": "identifier", "name": "FOO"}]),
+    ('FOO, BAR', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}]),
+    ('FOO, BAR, BAZ', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}, {"type": "identifier", "name": "BAZ"}]),
+    ('FOO, BAR, BAZ, QUUX', [{"type": "identifier", "name": "FOO"}, {"type": "identifier", "name": "BAR"}, {"type": "identifier", "name": "BAZ"}, {"type": "identifier", "name": "QUUX"}]),
+    ('FOO|BAR', [{"type": "bitset", "values": ["FOO", "BAR"]}]),
+    ('FOO|BAR|BAZ', [{"type": "bitset", "values": ["FOO", "BAR", "BAZ"]}]),
+    ('FOO|BAR, BAZ|QUUX', [{"type": "bitset", "values": ["FOO", "BAR"]}, {"type": "bitset", "values": ["BAZ", "QUUX"]}]),
+    ('0', [{"type": "int_b10", "value": 0}]),
+    ('123', [{"type": "int_b10", "value": 123}]),
+    ('-1234567', [{"type": "int_b10", "value": -1234567}]),
+    ('9223372036854775807', [{"type": "int_b10", "value": 9223372036854775807}]),
+    ('-9223372036854775808', [{"type": "int_b10", "value": -9223372036854775808}]),
+    ('0x0', [{"type": "uint_b16", "value": 0x0}]),
+    ('0x123', [{"type": "uint_b16", "value": 0x123}]),
+    ('0x1234567', [{"type": "uint_b16", "value": 0x1234567}]),
+    ('0x123456789abcdef0', [{"type": "uint_b16", "value": 0x123456789abcdef0}]),
+    ('"hello"', [{"type": "string", "value": "hello", "complete": True}]),
+    ('"FOO"', [{"type": "string", "value": "FOO", "complete": True}]),
+    ('"0asdf"', [{"type": "string", "value": "0asdf", "complete": True}]),
+    ('"qwer0asdf"', [{"type": "string", "value": "qwer0asdf", "complete": True}]),
+    ('""', [{"type": "string", "value": "", "complete": True}]),
+    ('"world"...', [{"type": "string", "value": "world", "complete": False}]),
+    ('""...', [{"type": "string", "value": "", "complete": False}]),
+    # This is a really wacky encoding.
+    (r'"really\1weird"', [{"type": "string", "value": "really\x01weird", "complete": True}]),
+    (r'"really\10weird"', [{"type": "string", "value": "really\x08weird", "complete": True}]),
+    (r'"really\100weird"', [{"type": "string", "value": "really\x40weird", "complete": True}]),
+    (r'"really\0010weird"', [{"type": "string", "value": "really\x010weird", "complete": True}]),
+    (r'"really\0100weird"', [{"type": "string", "value": "really\x080weird", "complete": True}]),
+    (r'"really\1000weird"', [{"type": "string", "value": "really\x400weird", "complete": True}]),
+    (r'"really\tweird"', [{"type": "string", "value": "really\x09weird", "complete": True}]),
+    (r'"really\nweird"', [{"type": "string", "value": "really\x0aweird", "complete": True}]),
+    (r'"really\vweird"', [{"type": "string", "value": "really\x0bweird", "complete": True}]),
+    (r'"really\fweird"', [{"type": "string", "value": "really\x0cweird", "complete": True}]),
+    (r'"really\rweird"', [{"type": "string", "value": "really\x0dweird", "complete": True}]),
+    (r'"really\"weird"', [{"type": "string", "value": "really\x22weird", "complete": True}]),
+    (r'"really\\weird"', [{"type": "string", "value": "really\x5cweird", "complete": True}]),
+    (r'"really\177weird"', [{"type": "string", "value": "really\x7fweird", "complete": True}]),
+    (r'"really\200weird"', [{"type": "string", "value": "really\x80weird", "complete": True}]),
+    (r'"really\234weird"', [{"type": "string", "value": "really\x9cweird", "complete": True}]),
+    (r'"really\345weird"', [{"type": "string", "value": "really\xe5weird", "complete": True}]),
+    (r'"really\377weird"', [{"type": "string", "value": "really\xffweird", "complete": True}]),
+    (r'"really\08weird"', [{"type": "string", "value": "really\x008weird", "complete": True}]),
+    (r'"really\18weird"', [{"type": "string", "value": "really\x018weird", "complete": True}]),
+    (r'"really\28weird"', [{"type": "string", "value": "really\x028weird", "complete": True}]),
+    (r'"really\38weird"', [{"type": "string", "value": "really\x038weird", "complete": True}]),
+    (r'"really\48weird"', [{"type": "string", "value": "really\x048weird", "complete": True}]),
+    (r'"really\58weird"', [{"type": "string", "value": "really\x058weird", "complete": True}]),
+    (r'"really\68weird"', [{"type": "string", "value": "really\x068weird", "complete": True}]),
+    (r'"really\78weird"', [{"type": "string", "value": "really\x078weird", "complete": True}]),
+    (r'"really\108weird"', [{"type": "string", "value": "really\x088weird", "complete": True}]),
+    (r'"really\208weird"', [{"type": "string", "value": "really\x108weird", "complete": True}]),
+    (r'"really\218weird"', [{"type": "string", "value": "really\x118weird", "complete": True}]),
+    (r'"sanity<check>"', [{"type": "string", "value": "sanity<check>", "complete": True}]),
+    (r'"sanity,check>"', [{"type": "string", "value": "sanity,check>", "complete": True}]),
+    (r'"sanity check>"', [{"type": "string", "value": "sanity check>", "complete": True}]),
+    (r'"sanity, check>"', [{"type": "string", "value": "sanity, check>", "complete": True}]),
+    # fdstrings without the string cannot possibly recognized as such. We shift that responsibility to the user.
+    (r'1', [{"type": "int_b10", "value": 1}]),
+    (r'1</dev/null>', [{"type": "fd", "value": 1, "path": "/dev/null", "metadata": None}]),
+    (r'1</dev/null<char 1:3>>', [{"type": "fd", "value": 1, "path": "/dev/null", "metadata": "char 1:3"}]),
+    (r'3</tmp/x/THE\"MARKER>', [{"type": "fd", "value": 3, "path": "/tmp/x/THE\"MARKER", "metadata": None}]),
+]
+
+NEGATIVES = [
+    '[',
+    ']',
+    '"',
+    ', ',
+    'FOO, ',
+    ', BAR',
+    'FOO,BAR',
+    '+7',
+    '-0x123',
+    '-3</tmp/foo>',
+    '0123',
+    '1234a',
+    '"hello\\"',
+    "'hello'",
+    '42...',
+    r'<barepath>',
+    r'<barepath<char 1:3>>',
+    r'"really\03weird"',
+    r'"really\8weird"',
+    "0x",
+]
+
+
+class TestArgListParserWorks(unittest.TestCase):
+    def test_positive(self):
+        for i, (input_string, expected) in enumerate(EXPECTATIONS):
+            with self.subTest(i=i, input_string=input_string):
+                tree = parse_arg.arg_list_parser.parse(input_string)
+                try:
+                    actual = parse_arg.TRANSFORMER.transform(tree)
+                except:
+                    print(f"{tree=}")
+                    print(f"pretty:{tree.pretty()}")
+                    raise
+                self.assertEqual(actual, expected)
+
+    def test_negative(self):
+        exceptions = (
+            lark.exceptions.UnexpectedCharacters,
+            lark.exceptions.UnexpectedToken,
+            lark.exceptions.UnexpectedEOF,
+            lark.exceptions.VisitError,
+        )
+        for i, input_string in enumerate(NEGATIVES):
+            with self.subTest(i=i, input_string=input_string):
+                with self.assertRaises(exceptions):
+                    tree = parse_arg.arg_list_parser.parse(input_string)
+                    built = parse_arg.TRANSFORMER.transform(tree)
+                    self.fail(f"{tree=} {built=}")
+
+
+if __name__ == "__main__":
+    unittest.main()
